b7faa49cf8140a207de22a97a810dac288ee931f
linh.nguyen5 <linh.nguyen5@CL-LINHNGUYEN5>
2018-04-13 18:11:35 +0700

    add code

---
+++ EpiserverCms.Web/Controllers/CommentUserBlockController.cs
@@ -0,0 +1,53 @@
+﻿using EPiServer.Core;
+using EPiServer.Data;
+using EPiServer.Data.Dynamic;
+using EPiServer.Personalization;
+using EPiServer.Web.Mvc;
+using EpiserverCms.Web.Models.Blocks;
+using EpiserverCms.Web.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EpiserverCms.Web.Controllers
+{
+
+    public class PersonWithIDynamicData : IDynamicData
+    {
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Identity Id { get; set; }
+    }
+
+    public class CommentUserBlockController : BlockController<CommentUserBlock>
+    {
+        // GET: CommentUserBlock
+        public override ActionResult Index(CommentUserBlock currentBlock)
+        {
+            EPiServerProfile currentUser = EPiServerProfile.Current;
+            var model = new CommentUserViewModel { Body = currentBlock.Body, User = currentUser.UserName };
+            //PageHelper => get current page
+
+            /*
+             *        DynamicDataStore store = DynamicDataStoreFactory.Instance.CreateStore("People", typeof(Person));
+            Identity id = store.Save(p);
+             */
+
+            var pageRouteHelper = EPiServer.ServiceLocation.ServiceLocator.Current.GetInstance<EPiServer.Web.Routing.IPageRouteHelper>();
+            var pageReference = pageRouteHelper.PageLink;
+            var id = pageReference.ID;
+
+            PersonWithIDynamicData newPerson = new PersonWithIDynamicData
+            {
+                Name = Guid.NewGuid().ToString(),
+                CreatedDate = DateTime.Now
+            };
+            //DynamicDataStore store = DynamicDataStoreFactory.Instance.CreateStore("Comment", typeof(PersonWithIDynamicData));
+            //var id = store.Save(newPerson);
+
+            return PartialView(model);
+        }
+    }
+}
